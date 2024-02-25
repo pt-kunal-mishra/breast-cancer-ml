1d62a69a6578a8cc741df49acf292d8bae088ce8
Kunal Kumar Mishra <98681856+pt-kunal-mishra@users.noreply.github.com>
2024-02-25 20:57:04 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+from flask import Flask,request,render_template
+import pickle
+
+app=Flask(__name__)
+
+model=pickle.load(open('breast.pickle','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST','GET'])
+def predict():
+    input_features=[float(x) for x in request.form.values()]
+    feature_values=[np.array(input_features)]
+
+    feature_name=['mean radius', 'mean texture', 'mean perimeter', 'mean area',
+       'mean smoothness', 'mean compactness', 'mean concavity',
+       'mean concave points', 'mean symmetry', 'mean fractal dimension',
+       'radius error', 'texture error', 'perimeter error', 'area error',
+       'smoothness error', 'compactness error', 'concavity error',
+       'concave points error', 'symmetry error', 'fractal dimension error',
+       'worst radius', 'worst texture', 'worst perimeter', 'worst area',
+       'worst smoothness', 'worst compactness', 'worst concavity',
+       'worst concave points', 'worst symmetry', 'worst fractal dimension']
+    
+
+    df=pd.DataFrame(feature_values,columns=feature_name)
+    output=model.predict(df)
+
+    if(output==0):
+        res_val="**breast cancer"
+    else:
+          res_val="**no breast cancer"  
+
+    return render_template('index.html',prediction_text="patient has {}".format(res_val))
+
+if __name__=="__main__":
+     app.run()      
